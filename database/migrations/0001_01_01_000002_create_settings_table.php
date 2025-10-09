aed6eacc4549ca52ba259a0cd6dc110fe6804b53
Nicholas Buttura <nicholas.buttura@8wave.it>
2025-10-09 15:58:31 +0200

    fix: fix down migration logic

--- database/migrations/0001_01_01_000002_create_settings_table.php
+++ database/migrations/0001_01_01_000002_create_settings_table.php
@@ -39,6 +39,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('setting_user');
         Schema::dropIfExists('settings');
     }
 };
