f61269d87451c3236eefcdd7fa311c84b0a2055b
Nicholas Buttura <nicholas.buttura@8wave.it>
2025-10-17 22:38:14 +0200

    fix: add missing method to mediaServiceInterface

--- src/Contracts/Api/MediaServiceInterface.php
+++ src/Contracts/Api/MediaServiceInterface.php
@@ -2,6 +2,7 @@
 
 namespace Wave8\Factotum\Base\Contracts\Api;
 
+use Illuminate\Database\Eloquent\Collection;
 use Wave8\Factotum\Base\Contracts\EntityServiceInterface;
 use Wave8\Factotum\Base\Dtos\Api\Media\StoreFileDto;
 use Wave8\Factotum\Base\Models\Media;
@@ -9,6 +10,6 @@ use Wave8\Factotum\Base\Models\Media;
 interface MediaServiceInterface extends EntityServiceInterface
 {
     public function store(StoreFileDto $data): Media;
-
     public function generateConversions(Media $media): void;
+    public function getMediaNotConverted(): Collection;
 }
