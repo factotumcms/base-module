19f33bd2a575ac7688cc0ea032c27b820ff7da9f
Nicholas Buttura <nicholas.buttura@8wave.it>
2025-10-28 17:10:42 +0100

    feat: add call static method on api response

--- src/Http/Responses/Api/ApiResponse.php
+++ src/Http/Responses/Api/ApiResponse.php
@@ -3,11 +3,11 @@
 namespace Wave8\Factotum\Base\Http\Responses\Api;
 
 use Illuminate\Http\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Str;
 
 final class ApiResponse extends JsonResponse
 {
-    public static function make(mixed $data, ?array $headers = null, int $status = Response::HTTP_OK): static
+    public static function make(mixed $data, ?array $headers = null, int $status = self::HTTP_OK): static
     {
         $response = new self(
             data: $data,
@@ -21,5 +21,27 @@ final class ApiResponse extends JsonResponse
         }
 
         return $response;
+    }
+
+    /**
+     * Handle dynamic static calls into the method based on the Http statuses.
+     */
+    public static function __callStatic($name, $arguments): static
+    {
+        if (Str::startsWith($name, 'Http')) {
+            $statusConstant = 'self::HTTP_'.Str::upper(Str::snake(Str::after($name, 'Http')));
+            if (defined($statusConstant)) {
+                $status = constant($statusConstant);
+                $headers = $arguments[0] ?? [];
+
+                return new static(
+                    data: null,
+                    status: $status,
+                    headers: $headers
+                );
+            }
+        }
+
+        throw new \BadMethodCallException("Method {$name} not supported.");
     }
 }
